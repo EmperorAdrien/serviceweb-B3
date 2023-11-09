6373be6a8dcb5992eae104e526e16993d7fdbb55
Loïc DANDOY <ldandoy@overconsulting.net>
2023-11-09 18:05:18 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
+from .params import PostTradParams
 
 app = FastAPI()
+
+@app.post('/trad')
+def postTrad(params: PostTradParams):
+    return {
+        'word': params.word,
+        'dictionnary': params.dictionnary,
+        'trad': "... --- ..."
+    }
 
 @app.get("/")
 def index():
     return {'msg': 'Hello World !'}
+
+@app.get("/trad/{word}")
+def getTrad(word: str):
+    return {
+        "word": word
+    }
